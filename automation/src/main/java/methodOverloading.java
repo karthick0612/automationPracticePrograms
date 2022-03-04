c3c5ade154ceec6f6e8bdb908c452450f6f581c9
vn52lsc <Karthick.S@walmart.com>
2022-03-04 15:52:41 +0530

    FirstCommit to Verify

---
+++ automation/src/main/java/methodOverloading.java
@@ -0,0 +1,8 @@
+public class methodOverloading {
+    public void method1(){
+        System.out.println("Automation");
+    }
+    public void method1(String str){
+        System.out.println(str);
+    }
+}
