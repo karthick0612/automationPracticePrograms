706149ae200a7b132ed00f074fb7bfa2f4a57fbe
Karthick <srinivasankarthick1@gmail.com>
2022-03-10 14:05:08 +0530

    Examples

---
+++ automation/src/main/java/collectionPrograms/Map_Programs.java
@@ -0,0 +1,13 @@
+package collectionPrograms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map_Programs {
+    public static void main(String[] args){
+        Map<String,String> exampleMap = new HashMap<>();
+        exampleMap.put("Karthick","Tester");
+        System.out.println(exampleMap);
+
+    }
+}
